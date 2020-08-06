ecdbf2ee56a649a0dc6ad010b6566f514791d06e
v-kazantsev <vvs201@mail.ru>
2020-08-06 23:07:35 +0700

    Updates

---
+++ precache-manifest.e959695a85da510107e8290745f388c0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03ef614313f05034b54a4149476871fa",
+    "url": "/Weather/index.html"
+  },
+  {
+    "revision": "ad6e48293537fa07893c",
+    "url": "/Weather/static/css/2.9c8c8187.chunk.css"
+  },
+  {
+    "revision": "01351588b9817fb45f53",
+    "url": "/Weather/static/css/main.9467222f.chunk.css"
+  },
+  {
+    "revision": "ad6e48293537fa07893c",
+    "url": "/Weather/static/js/2.0a22822b.chunk.js"
+  },
+  {
+    "revision": "a73b5a87fb564c29e725dbf7bea78d6d",
+    "url": "/Weather/static/js/2.0a22822b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01351588b9817fb45f53",
+    "url": "/Weather/static/js/main.6eeb2c4b.chunk.js"
+  },
+  {
+    "revision": "daa8c3d13753a2705eae",
+    "url": "/Weather/static/js/runtime-main.7db9982c.js"
+  }
+]);
